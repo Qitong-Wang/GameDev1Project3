0639fa03ed88714fbc99cc73598da00398e46c47
TonyWang98 <wqtls1998@gmail.com>
2018-10-29 13:55:48 -0400

    Players can move in a platform

---
+++ Sandwich/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+    public float speed = 6.0f;
+    private CharacterController _characterController;
+    public float gravity = -9.8f;
+	// Use this for initialization
+	void Start () {
+        _characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed); //Limits the maximum speed
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _characterController.Move(movement);
+	}
+}
