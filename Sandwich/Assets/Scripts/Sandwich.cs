bd95ae66beaa7f18934f3ba4133149c9eacb2e06
Sam Gould <drmcman67@yahoo.com>
2018-10-31 14:23:11 -0400

    added function to set destination

--- Sandwich/Assets/Scripts/Sandwich.cs
+++ Sandwich/Assets/Scripts/Sandwich.cs
@@ -7,6 +7,7 @@ using UnityEngine.AI;
 public class Sandwich : MonoBehaviour {
     public Vector3 goal;
     public NavMeshAgent agent;
+    public bool navigating;
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -15,6 +16,12 @@ public class Sandwich : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (!navigating) {
+            agent.destination = transform.position;
+        }
 	}
+    public void setDestination(Vector3 dest) {
+        agent.destination = dest;
+        navigating = true;
+    }
 }
