af927f3b12f228e545bd0c88b54614240b1e8ad3
Sam Gould <drmcman67@yahoo.com>
2018-10-31 16:01:06 -0400

    Auto stash before merge of "master" and "origin/master"

--- Sandwich/Assets/Scripts/TitleScreen.cs
+++ Sandwich/Assets/Scripts/TitleScreen.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class TitleScreen : MonoBehaviour {
+    public int currentPanel;
+    public List<Sprite> panels;
+    public GameObject button1, button2,button3, button4;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void TitleSlowStart() {
+        if (button2 != null)
+        {
+            Destroy(button2);
+            Destroy(button3);
+            Destroy(button1);
+            button4.SetActive(true);
+        }
+
+
+        if (currentPanel < panels.Count)
+        {
+            GetComponent<SpriteRenderer>().sprite = (panels[currentPanel]);
+            currentPanel++;
+        }
+        else {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void TitleFastStart() {
+        SceneManager.LoadScene(0);
+    }
+    public void TitleExitGame() {
+        Application.Quit();
+
+    }
 }
