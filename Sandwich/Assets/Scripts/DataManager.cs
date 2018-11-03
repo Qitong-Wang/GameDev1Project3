9021e363f7b084cd5914c6de2ce70b2dc93e9e58
TonyWang98 <wqtls1998@gmail.com>
2018-11-03 14:44:53 -0400

    Finish Kitchen Door

--- Sandwich/Assets/Scripts/DataManager.cs
+++ Sandwich/Assets/Scripts/DataManager.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManager : MonoBehaviour {
+public class DataManager : MonoBehaviour
+{
     public float cleanProgress = 0f; //Total percentage of clean
-    public float cleanGarbage = 0.5f; 
+    public float cleanGarbage = 0.5f;
     public int sandwichTotal = 6; //number of total sandwich
     public int sandwichIdle = 0; //number of sandwiches who are idle
     public int sandwichHolding = 6; //number of sandwiches player is holding
@@ -14,15 +15,17 @@ public class DataManager : MonoBehaviour {
     public List<GameObject> sandwichWaitingList;//Sandwich selection list
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         sandwichWaitingList = new List<GameObject>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void AddProgress()
     {
--- Sandwich/Assets/Scripts/TextManager.cs
+++ Sandwich/Assets/Scripts/TextManager.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 using System;
 using UnityEngine.SceneManagement;
 
-public class TextManager : MonoBehaviour {
+public class TextManager : MonoBehaviour
+{
     public GameObject DataManager; //DataManager
     DataManager dataManager;
     //Use for the Time text
-    public GameObject TimeText; 
+    public GameObject TimeText;
     Text timeText;
     public Slider ProgressSlider;
     public float second;
@@ -22,36 +23,38 @@ public class TextManager : MonoBehaviour {
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         timeText = TimeText.GetComponent<Text>();
         dataManager = DataManager.GetComponent<DataManager>();
         statusText = StatusText.GetComponent<Text>();
         this.UpdateSandwich();
-      
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+    // Update is called once per frame
+    void Update()
+    {
+
         //Time
         totalTime += Time.deltaTime;
         if (totalTime >= 1)
         {
             second--;
-            timeText.text = string.Format("Time Remain:\n {0:00}:{1:00}\n Working Progress:\n {2:00}%", ((int)second / 60), second % 60,((int)(dataManager.cleanProgress*100)));
+            timeText.text = string.Format("Time Remain:\n {0:00}:{1:00}\n Working Progress:\n {2:00}%", ((int)second / 60), second % 60, ((int)(dataManager.cleanProgress * 100)));
             totalTime = 0;
-          
+
         }
         //Slider
         ProgressSlider.value = dataManager.cleanProgress;
 
         //StatusText
-       }
+    }
     public void UpdateSandwich()
     {
         statusText.text = string.Format("Working Sandwiches: {0}/{1}\nIdle Sandwiches:{2}/{1}\nHolding Sandwiches:{3}/{1}"
            , dataManager.sandwichWorking, dataManager.sandwichTotal, dataManager.sandwichIdle, dataManager.sandwichHolding);
 
     }
-    
+
 }
